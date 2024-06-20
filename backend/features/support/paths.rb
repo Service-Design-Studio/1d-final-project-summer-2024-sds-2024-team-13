d01a24253c2e77564dd5767f3b9ac91435a6ee67
Hun Chong Tan <tanhunchong01@gmail.com>
2024-06-21 00:37:02 +0800

    done recent_transactions.feature

---
+++ backend/features/support/paths.rb
@@ -0,0 +1,21 @@
+module NavigationHelpers
+    def path_to(page_name)
+      case page_name
+      when /the home\s?page/
+        '/home'
+      when /the history page/
+        '/history'
+      when /the more page/
+        '/more'
+      when /the settings page/
+        '/settings'
+      when /the daily earnings settings page/
+        '/settings/daily_earnings'
+      else
+        raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
+          "Now, go and add a mapping in #{__FILE__}"
+      end
+    end
+  end
+  
+  World(NavigationHelpers)
