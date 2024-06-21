da8569538e80d8ddc6c04161ab42ed8513c75397
Noahhuang78 <huangziyi897@gmail.com>
2024-06-21 09:33:40 +0800

    l

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import './App.css';
+import "./App.css";
 
-
-
-import React from 'react';
-import { BrowserRouter as Route, Router } from 'react-router-dom';
-import List from './components/Listcomponent';
-import Navigator from './components/Navigator'; // Adjust path as per your file structure
-import Home from './Home'; // Import your Home component
-import Transactions from './Transactions'; // Import your Transactions component
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import List from "./components/Listcomponent";
+import Navigator from "./components/navigatorbar"; // Adjust path as per your file structure
+import Home from "./components/Home"; // Import your Home component
+import Transactions from "./components/Transactionpage"; // Import your Transactions component
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <div className="container">
-        <Navigator /> {/* Render the Navigator component */}
-        
-          <Route path="/" exact component={Home} /> {/* Route to Home component */}
-          <Route path="/transactions" component={Transactions} /> {/* Route to Transactions component */}
-        
-        <List /> {/* Render your List component */}
-      </div>
-    </Router>
+    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={Home} /> {/* Route to Home component */}
+        <Route path="/transactions" Component={Transactions} />{" "}
+        {/* Route to Transactions component */}
+      </Routes>
+    </BrowserRouter>
+    
   );
 };
 
---
+++ src/components/Home.tsx
@@ -0,0 +1,18 @@
+import './Home.css';
+import Header from './Header';
+import TopCard from './TopCard';
+import TopFive from './TopFive';
+import Navigator from './navigatorbar';
+
+const Home: React.FC = () => {
+  return (
+    <div className="App">
+      
+      <Header />
+      <TopCard />
+      <TopFive />
+    </div>
+  );
+}
+
+export default Home;
