806dc844d65c30085f8414ea56be78cc3542ea84
Rachel <rachel.limre03@gmail.com>
2024-07-05 11:40:44 +0800

    Edited User Model and Schema

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
     before_create :generate_user_id
-
+    self.primary_key = 'user_id'
+    
     private
     def generate_user_id
         self.user_id = loop do
           random_id = SecureRandom.hex(10)
+          Rails.logger.info "Generated user_id: #{random_id}"
           break random_id unless self.class.exists?(user_id: random_id)
         end
     end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_03_104624) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_05_031429) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "phone_num"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "user_id"
+    t.string "email"
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["user_id"], name: "index_users_on_user_id", unique: true
   end
 
