5347ab0ffa36523dd03f928f771cb1bc84cf2657
Rachel <rachel.limre03@gmail.com>
2024-07-08 09:59:30 +0800

    Updated request headers

--- frontend/src/utils/axiosConfig.js
+++ frontend/src/utils/axiosConfig.js
@@ -5,7 +5,8 @@ export const axiosInstance = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
     headers: {
         'Content-Type': 'application/json',
-        'Accept': 'application/json'  
+        'Accept': 'application/json',
+        'Access-Control-Allow-Origin': '*'  
     }
 });
 
