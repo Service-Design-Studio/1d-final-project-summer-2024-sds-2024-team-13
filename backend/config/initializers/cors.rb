22d1243350e2b138b53273e8e4cf7216a89ab2bc
Rachel <rachel.limre03@gmail.com>
2024-07-08 09:55:35 +0800

    added cors.rb

---
+++ backend/config/initializers/cors.rb
@@ -0,0 +1,11 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins 'http://localhost:3000', 'https://dbsbiz.as.r.appspot.com'
+      resource '*',
+        headers: :any,
+        expose: ['Access-Control-Allow-Origin'],
+        methods: [:get, :post, :put, :patch, :delete, :options],
+        credentials: true
+    end
+  end
+  
