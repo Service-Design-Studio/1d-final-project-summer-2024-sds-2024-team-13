4bcf10e40f081a28337fc9d50859e302eafc96f5
Rachel <rachel.limre03@gmail.com>
2024-07-09 17:16:30 +0800

    Added login screen to customer app

--- frontend/src/screens/HistoryScreen.js
+++ frontend/src/screens/HistoryScreen.js
@@ -1,9 +1,32 @@
+import { useCallback, useEffect, useState } from "react";
 import DropdownFilter from "../components/history/DropdownFilter";
 import HistoryNav from "../components/history/HistoryNav";
 import TransactionCard from "../components/history/TransactionCard";
+import { useAuth } from "../context/AuthContext";
 import styles from "../styles/history/History.module.css"
+import axiosInstance from "../utils/axiosConfig";
 
 const HistoryScreen = () => {
+    const { user } = useAuth();
+    const [transactions, setTransactions] = useState([]);
+    
+    const fetchTransactions = useCallback(async () => {
+        if (user) {
+            try {
+                const response = await axiosInstance.get(`/users/${user.user_id}/transactions`);
+                setTransactions(response.data
+                    .sort((a, b) => new Date(b.created_at) - new Date(a.created_at))
+                    .slice(0, 5));
+
+            } catch (error) {
+                console.error('Failed to fetch transactions:', error);
+            }
+        }
+    }, [user]);
+    useEffect(() => {
+        fetchTransactions();
+    }, [fetchTransactions]);
+
     return ( 
         <div className={styles.main}>
             <HistoryNav/>
--- frontend_customer/src/App.js
+++ frontend_customer/src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { BrowserRouter, Route, Routes, useLocation, useNavigate } from "react-router-dom";
+import { BottomNavigation, BottomNavigationAction } from '@mui/material';
+import { Home, Book, Payment, MoreHoriz } from '@mui/icons-material/';
+import LoginScreen from './screens/LoginScreen';
+
+function Navigation() {
+  const navigate = useNavigate();
+  const [screen, setScreen] = useState("home");
+
+  const handleScreen = (screen) => {
+    switch (screen) {
+      case "home":
+        navigate("/");
+        break;
+      case "history":
+        navigate("/");
+        break;
+      case "more":
+        navigate("/")
+        break;
+      default:
+        navigate("/");
+        break;
+    }
+    setScreen(screen);
+  };
+
+  return (
+    <BottomNavigation
+      showLabels
+      className='bottomNav'
+      value={screen}
+      onChange={(event, newScreen) => handleScreen(newScreen)}>
+      <BottomNavigationAction value="home" label="Home" icon={<Home />} />
+      <BottomNavigationAction value="payment" label="Payment" icon={<Payment />} />
+      <BottomNavigationAction value="history" label="History" icon={<Book />} />
+      <BottomNavigationAction value="more" label="More" icon={<MoreHoriz />} />
+    </BottomNavigation>
+  );
+}
+
+function ConditionalNavigation() {
+  const location = useLocation();
+
+  return (location.pathname !== '/' && location.pathname !== '/register') ? <Navigation /> : null;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="dimensionsWarning">
+        <h1>Please view the application in mobile mode</h1>
+      </div>
+      <BrowserRouter className="content">
+        <Routes>
+          <Route path="/" element={<LoginScreen />} />
+
+        </Routes>
+        <ConditionalNavigation />
+      </BrowserRouter>
     </div>
   );
 }
