b45b92e3bd0d39a1a35f60edb98bb96cadb5664c
yanzhenteo <115708948+yanzhenteo@users.noreply.github.com>
2024-07-11 15:20:38 +0800

    rspec testing with latest version, hit %90

--- backend/spec/requests/customer_transactions_spec.rb
+++ backend/spec/requests/customer_transactions_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.describe "CustomerTransactions", type: :request do
   let!(:user) { User.create!(name: 'Test User', email: 'user@example.com', password: 'password') }
   let!(:customer) { Customer.create!(phone_num: '1234567890', name: 'Test Customer', password: 'password') }
-  let!(:transaction) { Transaction.create!(customer_id: customer.customer_id, user_id: user.id, payment_method: 'credit', amount: 100.0) }
+  let!(:transaction) { Transaction.create!(customer_id: customer.id, user_id: user.id, payment_method: 'credit', amount: 100.0) }
 
   describe "GET /customers/:customer_id/transactions" do
     context "when customer_id is provided" do
       it "returns a success response and assigns transactions" do
-        get customer_customer_transactions_path(customer_id: customer.customer_id)
+        get customer_transactions_path(customer_id: customer.id)
         expect(response).to be_successful
         expect(assigns(:transactions)).to eq([transaction])
       end
@@ -25,7 +25,7 @@ RSpec.describe "CustomerTransactions", type: :request do
 
   describe "GET /customers/:customer_id/transactions/:id" do
     it "returns a success response" do
-      get customer_customer_transaction_path(customer_id: customer.customer_id, id: transaction.transaction_id)
+      get customer_transaction_path(customer_id: customer.id, id: transaction.id)
       expect(response).to be_successful
     end
   end
@@ -36,13 +36,13 @@ RSpec.describe "CustomerTransactions", type: :request do
 
       it "creates a new Transaction" do
         expect {
-          post customer_customer_transactions_path(customer_id: customer.customer_id), params: { transaction: valid_attributes }
+          post customer_transactions_path(customer_id: customer.id), params: { transaction: valid_attributes }
         }.to change(Transaction, :count).by(1)
       end
 
       it "redirects to the created transaction" do
-        post customer_customer_transactions_path(customer_id: customer.customer_id), params: { transaction: valid_attributes }
-        expect(response).to redirect_to(customer_customer_transaction_path(customer_id: customer.customer_id, id: Transaction.last.transaction_id))
+        post customer_transactions_path(customer_id: customer.id), params: { transaction: valid_attributes }
+        expect(response).to redirect_to(customer_transaction_path(customer_id: customer.id, id: Transaction.last.id))
       end
     end
 
@@ -51,12 +51,12 @@ RSpec.describe "CustomerTransactions", type: :request do
 
       it "does not create a new Transaction" do
         expect {
-          post customer_customer_transactions_path(customer_id: customer.customer_id), params: { transaction: invalid_attributes }
+          post customer_transactions_path(customer_id: customer.id), params: { transaction: invalid_attributes }
         }.to change(Transaction, :count).by(0)
       end
 
       it "renders the new template" do
-        post customer_customer_transactions_path(customer_id: customer.customer_id), params: { transaction: invalid_attributes }
+        post customer_transactions_path(customer_id: customer.id), params: { transaction: invalid_attributes }
         expect(response).to render_template(:new)
       end
     end
