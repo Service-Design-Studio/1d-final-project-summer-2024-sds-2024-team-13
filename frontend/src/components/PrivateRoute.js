87a677d8009d0865a40e57efe998d9ecef6c016b
Rachel <rachel.limre03@gmail.com>
2024-07-11 22:25:42 +0800

    Implemented persistent auth

--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -1,10 +1,14 @@
-// src/components/PrivateRoute.js
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 
 const PrivateRoute = () => {
-  const { user } = useAuth();  
+  const { user, loading } = useAuth();
+  
+  if (loading) {
+    return <div>Loading...</div>; // Or some other loading indicator
+  }
+
   return user ? <Outlet /> : <Navigate to="/" replace />;
 };
 
--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useState, useEffect, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { axiosInstance } from '../utils/axiosConfig'; 
+import axiosInstance from '../utils/axiosConfig'; 
 import { jwtDecode } from 'jwt-decode';
 
 const AuthContext = createContext();
@@ -9,12 +9,14 @@ export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true); 
   const navigate = useNavigate();
 
   const logout = useCallback(() => {
     localStorage.removeItem('token');
     setUser(null);
     navigate('/');
+    setLoading(false); 
   }, [navigate]);  
 
   const login = async (email, password) => {
@@ -22,7 +24,7 @@ export const AuthProvider = ({ children }) => {
       const response = await axiosInstance.post('users/login', { email, password });
       const { token } = response.data;
       localStorage.setItem('token', token);
-      await fetchUserProfile(token); 
+      await fetchUserProfile(token);
       navigate('/home');
       return true;
     } catch (error) {
@@ -33,25 +35,30 @@ export const AuthProvider = ({ children }) => {
 
   const fetchUserProfile = useCallback(async (token) => {
     try {
-      const decoded = jwtDecode(token); 
+      const decoded = jwtDecode(token);
       const response = await axiosInstance.get(`users/${decoded.user_id}`, {
         headers: { Authorization: `Bearer ${token}` }
       });
       setUser(response.data);
     } catch (error) {
       console.error('Failed to fetch user details:', error);
-      logout();  
+      logout();
+    } finally {
+      setLoading(false); 
     }
   }, [logout]);  
+
   useEffect(() => {
     const token = localStorage.getItem('token');
     if (token) {
-      fetchUserProfile(token);  
+      fetchUserProfile(token);
+    } else {
+      setLoading(false); 
     }
   }, [fetchUserProfile]); 
 
   return (
-    <AuthContext.Provider value={{ user, login, logout }}>
+    <AuthContext.Provider value={{ user, loading, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
