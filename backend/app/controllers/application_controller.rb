fca4f2214870fb16ef9defc03bf3e936ef312426
Rachel <rachel.limre03@gmail.com>
2024-07-11 22:49:56 +0800

    Added not found rescue

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery unless: -> { request.format.json? }
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+    private
+    def record_not_found
+        render json: { error: "Record not found" }, status: :not_found
+    end
 end
