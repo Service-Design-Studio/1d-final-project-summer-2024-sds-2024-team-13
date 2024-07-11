ce29a7f1bc14d1d6f727cc90f31ead4a0412cfcd
Rachel <rachel.limre03@gmail.com>
2024-07-11 16:24:02 +0800

    changed qr code expiry timing

--- frontend/src/components/payment/QRPay.js
+++ frontend/src/components/payment/QRPay.js
@@ -14,7 +14,7 @@ const QRPay = () => {
     useEffect(() => {
         const timer = setTimeout(() => {
             setIsExpired(true);
-        }, 10000); // 10 seconds for testing
+        }, 80000); // 10 seconds for testing
 
         return () => clearTimeout(timer);
     }, []);
