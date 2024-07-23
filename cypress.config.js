a5cccc7b71d5c59278d8e8865595d6ce06a835bd
Birthright00 <117997217+Birthright00@users.noreply.github.com>
2024-07-23 16:22:44 +0800

    Added Local and Deployed BaseURLS

--- cypress.config.js
+++ cypress.config.js
@@ -11,10 +11,22 @@ module.exports = defineConfig({
       });
       on('file:preprocessor', bundler);
       addCucumberPreprocessorPlugin(on, config);
+
+      // Set baseUrl based on the environment variable
+      const envBaseUrl = process.env.BASE_URL;
+      if (envBaseUrl) {
+        config.baseUrl = envBaseUrl;
+      }
+
       return config;
     },
     specPattern: 'cypress/e2e/**/*.feature',
     supportFile: 'cypress/support/e2e.js',
-    baseUrl: 'https://dbsbiz.as.r.appspot.com/', // Ensure this matches your development server URL
+    env: {
+      HAWKER_URL_LOCAL: 'http://localhost:3000',
+      CUSTOMER_URL_LOCAL: 'http://localhost:3001',
+      HAWKER_URL_DEPLOYED: 'https://dbsbiz.as.r.appspot.com/',
+      CUSTOMER_URL_DEPLOYED: 'https://dbsbiz-customer-dot-dbsbiz.as.r.appspot.com/'
+    }
   },
 });
