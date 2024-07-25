695ae372abbe7a05a3797a362cd5e614cbe44132
yanzhenteo <115708948+yanzhenteo@users.noreply.github.com>
2024-07-26 00:22:27 +0800

    refund request routing settled 96.08%

--- backend/spec/routing/refund_requests_routing_spec.rb
+++ backend/spec/routing/refund_requests_routing_spec.rb
@@ -7,35 +7,35 @@ RSpec.describe Customers::RefundRequestsController, type: :routing do
     end
 
     it 'routes to #new' do
-      expect(get: '/customers/1/transactions/1/refund_request/new').to route_to('customers/refund_requests#new', customer_id: '1', transaction_transaction_id: '1')
+      expect(get: '/customers/1/transactions/1/refund_request/new').to route_to('customers/refund_requests#new', customer_id: '1', transaction_id: '1')
     end
 
     it 'routes to #show' do
-      expect(get: '/customers/1/transactions/1/refund_request').to route_to('customers/refund_requests#show', customer_id: '1', transaction_transaction_id: '1')
+      expect(get: '/customers/1/transactions/1/refund_request').to route_to('customers/refund_requests#show', customer_id: '1', transaction_id: '1')
     end
 
     it 'routes to #edit' do
-      expect(get: '/customers/1/transactions/1/refund_request/edit').to route_to('customers/refund_requests#edit', customer_id: '1', transaction_transaction_id: '1')
+      expect(get: '/customers/1/transactions/1/refund_request/edit').to route_to('customers/refund_requests#edit', customer_id: '1', transaction_id: '1')
     end
 
     it 'routes to #create' do
-      expect(post: '/customers/1/transactions/1/refund_request').to route_to('customers/refund_requests#create', customer_id: '1', transaction_transaction_id: '1')
+      expect(post: '/customers/1/transactions/1/refund_request').to route_to('customers/refund_requests#create', customer_id: '1', transaction_id: '1')
     end
 
     it 'routes to #update via PUT' do
-      expect(put: '/customers/1/transactions/1/refund_request').to route_to('customers/refund_requests#update', customer_id: '1', transaction_transaction_id: '1')
+      expect(put: '/customers/1/transactions/1/refund_request').to route_to('customers/refund_requests#update', customer_id: '1', transaction_id: '1')
     end
 
     it 'routes to #update via PATCH' do
-      expect(patch: '/customers/1/transactions/1/refund_request').to route_to('customers/refund_requests#update', customer_id: '1', transaction_transaction_id: '1')
+      expect(patch: '/customers/1/transactions/1/refund_request').to route_to('customers/refund_requests#update', customer_id: '1', transaction_id: '1')
     end
 
     it 'routes to #destroy' do
-      expect(delete: '/customers/1/transactions/1/refund_request').to route_to('customers/refund_requests#destroy', customer_id: '1', transaction_transaction_id: '1')
+      expect(delete: '/customers/1/transactions/1/refund_request').to route_to('customers/refund_requests#destroy', customer_id: '1', transaction_id: '1')
     end
 
     it 'routes to #update_status' do
-      expect(patch: '/customers/1/transactions/1/refund_request/update_status').to route_to('customers/refund_requests#update_status', customer_id: '1', transaction_transaction_id: '1')
+      expect(patch: '/customers/1/transactions/1/refund_request/update_status').to route_to('customers/refund_requests#update_status', customer_id: '1', transaction_id: '1')
     end
   end
 end
