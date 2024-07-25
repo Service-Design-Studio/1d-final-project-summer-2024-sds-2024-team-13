c076e42398c40eeabd0641c1b4c2e5412e822ba9
Birthright00 <117997217+Birthright00@users.noreply.github.com>
2024-07-26 04:24:06 +0800

    Added and Change Features

--- cypress/e2e/sprint3/Customer Raise Refund First/customer_refund_system/happy/Customer-Happy.feature
+++ cypress/e2e/sprint3/Customer Raise Refund First/customer_refund_system/happy/Customer-Happy.feature
@@ -8,10 +8,8 @@ Feature: Happy Path, Customer View
         Then I should see how much I've paid
         Then I am redirected to Request Refund view
         And I should see the amount I paid, who I paid to, my phone number, date and time of transaction, transaction ID, expected payment from customer, reason for dispute and amount to be refunded
-        When I click on Request Refund button on the Popup
         Then I fill in the respective fields for expected payment from customer
         And I click on Submit button
         Then I see a notification at the bottom saying "SUBMITTED!"
         Then I should be redirected to Transaction History View
-        And I should see that transaction card become red
-        And a notification update on my Request Refund button
+        And I should see that transaction card becomes yellow
--- cypress/e2e/sprint3/Customer Raise Refund First/customer_refund_system/happy/Customer-Happy.js
+++ cypress/e2e/sprint3/Customer Raise Refund First/customer_refund_system/happy/Customer-Happy.js
@@ -29,10 +29,15 @@ Then("I should see how much I've paid", () => {
   cy.get('[data-testid="transaction-amount"]').should('be.visible');
 });
 
+Then("I click on Request Refund button on the Popup", () => {
+  cy.get('[data-testid="refund-customer-button"]').click();
+});
+
 Then("I am redirected to Request Refund view", () => {
   cy.get('[data-testid="refund-customer-button"]').click();
   cy.url().should('include', '/refunds/request');
 });
+
 
 Then("I should see the amount I paid, who I paid to, my phone number, date and time of transaction, transaction ID, expected payment from customer, reason for dispute and amount to be refunded", () => {
   cy.get('[data-testid="refund-amount"]').should('be.visible');
@@ -40,14 +45,12 @@ Then("I should see the amount I paid, who I paid to, my phone number, date and t
   cy.get('[data-testid="refund-customer-mobile"]').should('be.visible');
   cy.get('[data-testid="refund-timestamp"]').should('be.visible');
   cy.get('[data-testid="refund-transaction-id"]').should('be.visible');
-  cy.get('[data-testid="refund-expected-payment"]').should('be.visible');
-  cy.get('[data-testid="refund-reasons"]').should('be.visible');
-  cy.get('[data-testid="refund-amount-to-be-refunded"]').should('be.visible');
+  cy.get('[data-testid="expected-payment-input"]').should('be.visible');
+  cy.get('[data-testid="expected-refund-input"]').should('be.visible');
+  cy.get('[data-testid="refund-reason-input"]').should('be.visible');
 });
 
-When("I click on Request Refund button on the Popup", () => {
-  cy.get('[data-testid="refund-customer-button"]').click();
-});
+
 
 Then("I fill in the respective fields for expected payment from customer", () => {
   cy.get('[data-testid="expected-payment-input"]').type('1'); // Example value
@@ -63,13 +66,11 @@ Then("I see a notification at the bottom saying {string}", (notification) => {
 });
 
 Then("I should be redirected to Transaction History View", () => {
+  cy.get('[data-testid="back-button"]').click();
   cy.url().should('include', '/history');
 });
 
-Then("I should see that transaction card become red", () => {
-  cy.get('[data-testid^="transaction-card"]').first().should('have.css', 'border-left-color', 'rgb(255, 0, 0)'); // Assuming red color indicates a dispute
+Then("I should see that transaction card becomes yellow", () => {
+  cy.get('[data-testid^="transaction-card"]').should('contain', 'REFUND REQUESTED');
 });
-
-Then("a notification update on my Request Refund button", () => {
-  cy.get('[data-testid="refund-notification"]').should('be.visible');
-});
+  
