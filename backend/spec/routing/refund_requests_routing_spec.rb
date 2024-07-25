bcf94faf26673d7fca87b796ee5092b124e42318
yanzhenteo <115708948+yanzhenteo@users.noreply.github.com>
2024-07-25 14:16:45 +0800

    start rspec, 86% code coverage

--- backend/spec/routing/refund_requests_routing_spec.rb
+++ backend/spec/routing/refund_requests_routing_spec.rb
@@ -1,38 +1,37 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe RefundRequestsController, type: :routing do
-  describe "routing" do
-    it "routes to #index" do
-      expect(get: "/refund_requests").to route_to("refund_requests#index")
+RSpec.describe Customers::RefundRequestsController, type: :routing do
+  describe 'routing' do
+    it 'routes to #index' do
+      expect(get: '/customers/1/refund_requests').to route_to('customers/refund_requests#index', customer_id: '1')
     end
 
-    it "routes to #new" do
-      expect(get: "/refund_requests/new").to route_to("refund_requests#new")
+    it 'routes to #new' do
+      expect(get: '/customers/1/refund_requests/new').to route_to('customers/refund_requests#new', customer_id: '1')
     end
 
-    it "routes to #show" do
-      expect(get: "/refund_requests/1").to route_to("refund_requests#show", id: "1")
+    it 'routes to #show' do
+      expect(get: '/customers/1/refund_requests/1').to route_to('customers/refund_requests#show', customer_id: '1', id: '1')
     end
 
-    it "routes to #edit" do
-      expect(get: "/refund_requests/1/edit").to route_to("refund_requests#edit", id: "1")
+    it 'routes to #edit' do
+      expect(get: '/customers/1/refund_requests/1/edit').to route_to('customers/refund_requests#edit', customer_id: '1', id: '1')
     end
 
-
-    it "routes to #create" do
-      expect(post: "/refund_requests").to route_to("refund_requests#create")
+    it 'routes to #create' do
+      expect(post: '/customers/1/refund_requests').to route_to('customers/refund_requests#create', customer_id: '1')
     end
 
-    it "routes to #update via PUT" do
-      expect(put: "/refund_requests/1").to route_to("refund_requests#update", id: "1")
+    it 'routes to #update via PUT' do
+      expect(put: '/customers/1/refund_requests/1').to route_to('customers/refund_requests#update', customer_id: '1', id: '1')
     end
 
-    it "routes to #update via PATCH" do
-      expect(patch: "/refund_requests/1").to route_to("refund_requests#update", id: "1")
+    it 'routes to #update via PATCH' do
+      expect(patch: '/customers/1/refund_requests/1').to route_to('customers/refund_requests#update', customer_id: '1', id: '1')
     end
 
-    it "routes to #destroy" do
-      expect(delete: "/refund_requests/1").to route_to("refund_requests#destroy", id: "1")
+    it 'routes to #destroy' do
+      expect(delete: '/customers/1/refund_requests/1').to route_to('customers/refund_requests#destroy', customer_id: '1', id: '1')
     end
   end
 end
