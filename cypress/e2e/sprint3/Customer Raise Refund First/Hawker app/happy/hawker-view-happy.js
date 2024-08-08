98ebe66eea71a879824b81e2ee8e741bffa14274
Birthright00 <117997217+Birthright00@users.noreply.github.com>
2024-08-08 13:36:00 +0800

    Added TimeOut Changes to My Branch

--- cypress/e2e/sprint3/Customer Raise Refund First/Hawker app/happy/hawker-view-happy.js
+++ cypress/e2e/sprint3/Customer Raise Refund First/Hawker app/happy/hawker-view-happy.js
@@ -3,8 +3,8 @@ import { Before, Given, When, Then } from "@badeball/cypress-cucumber-preprocess
 Before(() => {
   cy.viewport('iphone-6+');
   cy.visit("/");
-  cy.get('input[placeholder="Email"]').type('chicken@gmail.com');
-  cy.get('input[placeholder="Password"]').type('123');
+  cy.get('input[placeholder="Email"]', { timeout: 10000 }).type('higoogle@gmail.com');
+  cy.get('input[placeholder="Password"]', { timeout: 10000 }).type('123');
   cy.contains("LOG IN").click();
   cy.contains("DBSBiz", { timeout: 10000 }).should('be.visible');
 });
@@ -20,7 +20,7 @@ Given("I am on Refund Request view", () => {
 });
 
 When("I click on transaction card that contains {string}", (status) => {
-  cy.get('[data-testid^="refund-card"]')
+  cy.get('[data-testid^="refund-card"]', { timeout: 10000 })
     .first()
     .should('be.visible')
     .click({ force: true });
@@ -31,15 +31,15 @@ Then("I should be redirected to Refund Details view of the transaction card", ()
 });
 
 Then("I should see decline button", () => {
-  cy.get('[data-testid="decline-button"]').should('be.visible');
+  cy.get('[data-testid="decline-button"]', { timeout: 10000 }).should('be.visible');
 });
 
 Then("I should see accept button", () => {
-  cy.get('[data-testid="accept-button"]').should('be.visible');
+  cy.get('[data-testid="accept-button"]', { timeout: 10000 }).should('be.visible');
 });
 
 Then("I click on the Accept button", () => {
-    cy.get('[data-testid="accept-button"]').click();
+    cy.get('[data-testid="accept-button"]', { timeout: 10000 }).click();
   });
   
 
@@ -48,26 +48,26 @@ Then("I click on the Accept button", () => {
   });
 
   Then("I clicked on the back button", () => {
-    cy.get('[data-testid="back-button"]')
+    cy.get('[data-testid="back-button"]', { timeout: 10000 })
     .first()
     .click({ force: true });
 });
 
   Then("I should see a new transaction card with a deduction and word saying “REFUNDED”", () => {
-    cy.get('[data-testid^="transaction-card"]')
+    cy.get('[data-testid^="transaction-card"]', { timeout: 10000 })
       .first()
       .should('contain', 'REFUNDED')
     });
    
   Then("I click on Requested Refund button", () => {
-    cy.get('[data-testid="requested-refunds-button"]').click();
+    cy.get('[data-testid="requested-refunds-button"]', { timeout: 10000 }).click();
       });
   Then("Click on Refunded Tab in Requested Refunds view", () => {
-    cy.get('[data-testid="refunded-tab"]').click();
+    cy.get('[data-testid="refunded-tab"]', { timeout: 10000 }).click();
       });
 
   Then("I should see the refunded transaction in the Refunded Tab", () => {
-    cy.get('[data-testid^="refund-card"]')
+    cy.get('[data-testid^="refund-card"]', { timeout: 10000 })
       .first()
       .should('contain', 'REFUNDED');
     });
